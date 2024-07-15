6f19ea511459682c351f6dd295a080cc409985f6
fzhnf <faizahnaf52@gmail.com>
2024-07-15 20:22:39 +0800

    feat: add auth middleware to logout route

--- start/routes.ts
+++ start/routes.ts
@@ -9,6 +9,7 @@
 
 const AuthController = () => import('#controllers/auth_controller')
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
 router.get('/', async () => {
   return {
@@ -30,7 +31,7 @@ router
       .group(() => {
         router.post('/register', [AuthController, 'register']).as('register')
         router.post('/login', [AuthController, 'login']).as('login')
-        router.delete('/logout', [AuthController, 'logout']).as('logout')
+        router.delete('/logout', [AuthController, 'logout']).as('logout').use(middleware.auth())
 
         router.get('/me', [AuthController, 'me']).as('me')
       })
