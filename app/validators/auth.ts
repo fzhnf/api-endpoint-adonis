6d6c3b3c76e846ba540bdd178bb5fa60cf959ec5
fzhnf <faizahnaf52@gmail.com>
2024-07-15 22:02:04 +0800

    feat: add email existence check in loginValidator

--- app/validators/auth.ts
+++ app/validators/auth.ts
@@ -18,7 +18,14 @@ export const registerValidator = vine.compile(
 
 export const loginValidator = vine.compile(
   vine.object({
-    email: vine.string().email().normalizeEmail(),
+    email: vine
+      .string()
+      .email()
+      .normalizeEmail()
+      .exists(async (db, value) => {
+        const match = await db.from('users').select('id').where('email', value).first()
+        return !!match
+      }),
     password: vine.string().trim().minLength(8),
   })
 )
