1f2aa61432534afefc1620972cd64c835b754651
fzhnf <faizahnaf52@gmail.com>
2024-07-16 21:55:14 +0800

    refactor: group thread routes under prefix `/api`

--- start/routes.ts
+++ start/routes.ts
@@ -30,12 +30,13 @@ router
       })
       .prefix('/auth')
       .as('auth')
+
+    router.get('/threads', [ThreadController, 'index']).as('threads.index')
+    router.post('/threads', [ThreadController, 'store']).as('threads.store').use(middleware.auth())
+    router.get('/threads/:id', [ThreadController, 'show']).as('threads.show')
+    router
+      .put('/threads/:id', [ThreadController, 'update'])
+      .as('threads.update')
+      .use(middleware.auth())
   })
   .prefix('/api')
-
-router.get('/threads', [ThreadController, 'index']).as('threads.index')
-
-router.post('/threads', [ThreadController, 'store']).as('threads.store').use(middleware.auth())
-
-router.get('/threads/:id', [ThreadController, 'show']).as('threads.show')
-router.put('/threads/:id', [ThreadController, 'update']).as('threads.update').use(middleware.auth())
