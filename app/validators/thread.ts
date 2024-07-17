ca76a567d4f5dc71eeae6487a10ad360b9bca2b6
fzhnf <faizahnaf52@gmail.com>
2024-07-17 20:50:46 +0800

    refactor: update title maxLength to 255 in thread validators

--- app/validators/thread.ts
+++ app/validators/thread.ts
@@ -6,7 +6,7 @@ import vine from '@vinejs/vine'
  */
 export const createThreadValidator = vine.compile(
   vine.object({
-    title: vine.string().trim().maxLength(254),
+    title: vine.string().trim().maxLength(255),
     content: vine.string().trim(),
     categoryId: vine.number().exists(async (db, value) => {
       const match = await db.from('categories').select('id').where('id', value).first()
@@ -21,7 +21,7 @@ export const createThreadValidator = vine.compile(
  */
 export const updateThreadValidator = vine.compile(
   vine.object({
-    title: vine.string().trim().maxLength(254).optional(),
+    title: vine.string().trim().maxLength(255).optional(),
     content: vine.string().trim().optional(),
     categoryId: vine
       .number()
