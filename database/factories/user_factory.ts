6d8c5a5f881afc2d88c7d82486b8ea0050d4ac17
fzhnf <faizahnaf52@gmail.com>
2024-07-25 13:52:42 +0800

    fix(user_factory): ensure email normalization during user creation, bug fix

--- database/factories/user_factory.ts
+++ database/factories/user_factory.ts
@@ -1,15 +1,18 @@
 import factory from '@adonisjs/lucid/factories'
 import User from '#models/user'
 import { ThreadFactory } from './thread_factory.js'
+import validator from "validator";
 
 export const UserFactory = factory
   .define(User, async ({ faker }) => {
     return {
-      username: faker.internet.userName(),
-      fullName: faker.person.fullName(),
-      email: faker.internet.email(),
       password: '12341234',
     }
   })
   .relation('threads', () => ThreadFactory)
   .build()
+		const email = faker.internet.email();
+		const normalizedEmail = validator.normalizeEmail(email) || email; // Fallback to original email if normalization fails
+			username: faker.internet.userName(),
+			fullName: faker.person.fullName(),
+			email: normalizedEmail,
