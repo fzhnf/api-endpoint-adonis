69d55e7fbd96fa1d33cddec736bba5e35e0e8652
fzhnf <faizahnaf52@gmail.com>
2024-07-25 14:05:47 +0800

    feat(seeder): expand seeding to include replies and increase counts

--- database/seeders/00_fake_seeder.ts
+++ database/seeders/00_fake_seeder.ts
@@ -1,21 +1,29 @@
-import { CategoryFactory } from '#database/factories/category_factory'
-import { ThreadFactory } from '#database/factories/thread_factory'
-import { UserFactory } from '#database/factories/user_factory'
-import Category from '#models/category'
-import User from '#models/user'
-import { BaseSeeder } from '@adonisjs/lucid/seeders'
+import { CategoryFactory } from "#database/factories/category_factory";
+import { ThreadFactory } from "#database/factories/thread_factory";
+import { UserFactory } from "#database/factories/user_factory";
+import Category from "#models/category";
+import User from "#models/user";
+import { BaseSeeder } from "@adonisjs/lucid/seeders";
 
 export default class extends BaseSeeder {
-  async run() {
-    // Write your database queries inside the run method
-    const category = await CategoryFactory.createMany(5)
-    const user = await UserFactory.createMany(2)
-    await this.#createThreads(category, user)
-  }
-  async #createThreads(category: Category[], user: User[]) {
-    await ThreadFactory.tap((row) => {
-      row.categoryId = category.at(Math.floor(Math.random() * category.length))!.id
-      row.userId = user.at(Math.floor(Math.random() * user.length))!.id
-    }).createMany(10)
-  }
+	async run() {
+		const category = await CategoryFactory.createMany(8);
+		const user = await UserFactory.createMany(4);
+		await this.#createThreadsAndReplies(category, user);
+	}
+	async #createThreadsAndReplies(category: Category[], user: User[]) {
+		const thread = await ThreadFactory.tap((row) => {
+			row.categoryId = category.at(
+				Math.floor(Math.random() * category.length),
+			)!.id;
+			row.userId = user.at(Math.floor(Math.random() * user.length))!.id;
+		})
+			.with("replies", 4, (reply) =>
+				reply.tap((row) => {
+					row.userId = user.at(Math.floor(Math.random() * user.length))!.id;
+				}),
+			)
+			.createMany(16);
+		return thread;
+	}
 }
