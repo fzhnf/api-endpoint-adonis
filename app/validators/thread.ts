bee58f994c65f5c06cf96006c30969430c5fd1ca
fzhnf <faizahnaf52@gmail.com>
2024-07-25 18:14:38 +0800

    style: reformat and align validator code

--- app/validators/thread.ts
+++ app/validators/thread.ts
@@ -5,30 +5,38 @@ import vine from '@vinejs/vine'
  * a new thread.
  */
 export const createThreadValidator = vine.compile(
-  vine.object({
-    title: vine.string().trim().maxLength(255),
-    content: vine.string().trim(),
-    categoryId: vine.number().exists(async (db, value) => {
-      const match = await db.from('categories').select('id').where('id', value).first()
-      return !!match
-    }),
-  })
-)
+	vine.object({
+		title: vine.string().trim().maxLength(255),
+		content: vine.string().trim(),
+		categoryId: vine.number().exists(async (db, value) => {
+			const match = await db
+				.from("categories")
+				.select("id")
+				.where("id", value)
+				.first();
+			return !!match;
+		}),
+	}),
+);
 
 /**
  * Validator to validate the payload when updating
  * an existing thread.
  */
 export const updateThreadValidator = vine.compile(
-  vine.object({
-    title: vine.string().trim().maxLength(255).optional(),
-    content: vine.string().trim().optional(),
-    categoryId: vine
-      .number()
-      .exists(async (db, value) => {
-        const match = await db.from('categories').select('id').where('id', value).first()
-        return !!match
-      })
-      .optional(),
-  })
-)
+	vine.object({
+		title: vine.string().trim().maxLength(255).optional(),
+		content: vine.string().trim().optional(),
+		categoryId: vine
+			.number()
+			.exists(async (db, value) => {
+				const match = await db
+					.from("categories")
+					.select("id")
+					.where("id", value)
+					.first();
+				return !!match;
+			})
+			.optional(),
+	}),
+);
