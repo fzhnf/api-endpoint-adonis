4c783192093e22911d14d101cdbc9a63fdef92ed
fzhnf <faizahnaf52@gmail.com>
2024-07-25 14:03:49 +0800

    chore: remove unused seeders

--- database/seeders/category_seeder.ts
+++
@@ -1,9 +0,0 @@
-import { CategoryFactory } from '#database/factories/factory'
-import { BaseSeeder } from '@adonisjs/lucid/seeders'
-
-export default class extends BaseSeeder {
-  async run() {
-    // Write your database queries inside the run method
-    await CategoryFactory.createMany(5)
-  }
-}
--- database/seeders/thread_seeder.ts
+++
@@ -1,9 +0,0 @@
-import { ThreadFactory } from '#database/factories/factory'
-import { BaseSeeder } from '@adonisjs/lucid/seeders'
-
-export default class extends BaseSeeder {
-  async run() {
-    // Write your database queries inside the run method
-    await ThreadFactory.createMany(10)
-  }
-}
