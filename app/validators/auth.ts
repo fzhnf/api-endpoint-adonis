4d6dcce80ea82c22ab3fd7a5a891b0250c849e4a
fzhnf <faizahnaf52@gmail.com>
2024-07-25 14:02:20 +0800

    fix(auth_validator): refactor and enhance validation logic

--- app/validators/auth.ts
+++ app/validators/auth.ts
@@ -1,20 +1,34 @@
 import vine from '@vinejs/vine'
 
 export const registerValidator = vine.compile(
-  vine.object({
-    username: vine.string().trim(),
-    fullName: vine.string().trim().optional(),
-    email: vine
-      .string()
-      .email()
-      .normalizeEmail()
-      .unique(async (db, value) => {
-        const match = await db.from('users').select('id').where('email', value).first()
-        return !match
-      }),
-    password: vine.string().trim().minLength(8).confirmed(),
-  })
-)
+	vine.object({
+		username: vine
+			.string()
+			.trim()
+			.unique(async (db, value) => {
+				const match = await db
+					.from("users")
+					.select("id")
+					.where("username", value)
+					.first();
+				return !match;
+			}),
+		fullName: vine.string().trim().optional(),
+		email: vine
+			.string()
+			.email()
+			.normalizeEmail()
+			.unique(async (db, value) => {
+				const match = await db
+					.from("users")
+					.select("id")
+					.where("email", value)
+					.first();
+				return !match;
+			}),
+		password: vine.string().trim().minLength(8).confirmed(),
+	}),
+);
 
 export const loginValidator = vine.compile(
   vine.object({
